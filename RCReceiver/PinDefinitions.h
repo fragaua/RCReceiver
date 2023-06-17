01be3b071fa79b05beaccc841dda3b33d23f3a08
Fraga <fraga16@ua.pt>
2023-06-17 15:43:38 +0100

    First commit after separation from ferrari. Added channel support

---
+++ RCReceiver/PinDefinitions.h
@@ -0,0 +1,30 @@
+/**
+ * @file PinDefinitions.h
+ * @author Marcelo Fraga
+ * @brief Arduino pinout definition for the receiver module of the Ferrari RC Project.
+ *        This is project specific. There is a different pinout definition for the transmitter part.
+ * @version 0.1
+ * @date 2022-11-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+
+#ifndef PINDEFINITIONS_H
+#define PINDEFITIONS_H
+
+/* 
+ *  PIN Definitions  
+ */
+
+
+
+#define RF24_CSN_PIN              8  // When CSN is low, module listens on SPI port
+#define RF24_CE_PIN               9  // CE Selects whether transmit or receive
+#define RF24_MOSI_PIN             11 // SPI input. Used to receive data from uC
+#define RF24_MISO_PIN             12 // SPI output. Used to send data to uC
+#define RF24_SCK_PIN              13 // Serial clock
+
+
+#endif
---
+++ RCReceiver/RCReceiver.ino
@@ -0,0 +1,106 @@
+// #include <printf.h>
+#include "PinDefinitions.h"
+#include <RF24.h>
+#include <Arduino.h>
+#include <Servo.h>
+
+
+/*
+* NRF24L01 RFCom related
+*/
+
+#define DEBUG_MODE   0 // Debug mode prints all variables from the received payload.
+#define NORMAL_MODE  1 
+
+// Define what is the running mode.
+#define RUN_MODE NORMAL_MODE
+
+#define RF_ADDRESS_SIZE 6
+const byte RF_Address[RF_ADDRESS_SIZE] = "1Node";
+uint8_t address[][6] = { "1Node"};
+
+
+#define N_CHANNELS 9u // Channels are exactly the same as the remote here. The physical outputs however, might be less
+
+typedef struct RFPayload{
+  uint16_t u16_Channels[N_CHANNELS];
+}RFPayload;
+
+
+
+RF24 radio(9, 8); // use a different set of signals so we liberate one of the PWM outputs
+Servo servo;
+
+RFPayload payload;
+
+
+void setup() {
+   Serial.begin(115200);
+  while (!Serial) {
+    // some boards need to wait to ensure access to serial over USB
+  }
+
+  // initialize the transceiver on the SPI bus
+  if (!radio.begin()) {
+    Serial.println(F("radio hardware is not responding!!"));
+    while (1) {}  // hold in infinite loop
+  }
+  radio.setPALevel(RF24_PA_LOW);
+  radio.setPayloadSize(sizeof(RFPayload));
+  //radio.openWritingPipe(address[0]);
+  radio.openReadingPipe(0, RF_Address);
+  radio.startListening();
+
+//  printf_begin();
+  radio.printDetails();
+  servo.attach(3);
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+}
+
+int i = 0;
+int cnt = 0;
+uint16_t motor_run, servo_turn;
+void loop() {
+   // TODO: Use internal LED to give some information on the communication state
+    uint8_t pipe;
+    if (radio.available(&pipe)) {              // is there a payload? get the pipe number that recieved it
+      uint8_t bytes = radio.getPayloadSize();  // get the size of the payload
+      radio.read(&payload, bytes);             // fetch payload from FIFO
+      // Serial.print(F("Received "));
+      // Serial.print(bytes);  // print the size of the payload
+      // Serial.println();
+      // Serial.print(F(" bytes on pipe "));
+      //Serial.print(pipe);  // print the pipe number
+      //Serial.print(F(": "));
+      if(cnt % 20 == 0)
+      {
+        uint8_t i;
+        for(i = 0; i < N_CHANNELS; i++)
+        {
+          Serial.print("Channel ");
+          Serial.print(i);
+          Serial.print(" :");
+          Serial.println(payload.u16_Channels[i]);
+        }
+          Serial.println("\n\n\n");
+      }
+      cnt++;
+    }
+    else{
+      // Serial.println("Com Broken");
+      // delay(1000);
+      // payload.d_Joystick_Left.u16_Value_X = 509;
+    }
+//
+    servo_turn = map(payload.u16_Channels[1], 0, 1023, 0, 180);
+    // Serial.print("Servo:");
+    // Serial.print(servo_turn);
+    // Serial.print("\t");
+    servo.write(servo_turn);
+
+
+   
+    delay(50);
+  
+}
